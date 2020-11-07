56827e75a2871395222fcc9f5a08bb43a73511ff
Eduardo Rodriguez <lalorodrigez538@gmail.com>
2020-11-07 13:15:49 -0500

    mapa con marcador

--- src/views/Ubicanos.vue
+++ src/views/Ubicanos.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d493.75481893359546!2d-79.02824907096797!3d-8.097551098195597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2spe!4v1604772158513!5m2!1ses-419!2spe" width="100%" height="500" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d987.5097038275718!2d-79.02853227082159!3d-8.097524199635641!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91ad3d8dab870cdf%3A0x39a817ec98f8bb4!2sJose%20E.%20Rodo%20686%2C%20Trujillo%2013001!5e0!3m2!1ses-419!2spe!4v1604772843092!5m2!1ses-419!2spe" width="100%" height="600" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </template>
 
 <script>
